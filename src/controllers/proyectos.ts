83059431cdb14fe69d051e62014db71d4cd3c034
puppy09 <zepedaramireztics@gmail.com>
2024-09-19 23:31:47 -0700

    CRUD Proyectos y CRUD Alternativas

--- src/controllers/proyectos.ts
+++ src/controllers/proyectos.ts
@@ -16,11 +16,84 @@ const postProyecto = async (req:Request, res:Response)=>{
             objetivo
         });
 
-        res.status(201).json(newProyecto)
+        return res.status(201).json(newProyecto)
     }catch(error){
         console.error("Error creando proyecto", error);
         console.error("ERROR POSTEANDO PROYECTO");
     }
 }
 
-export{ postProyecto }
+
+//UPDATE Proyecto
+const updateProyecto = async (req:Request, res:Response)=>{
+    try{
+
+        //Obtenemos ID y parmetros del body
+        const{ proyecto_id }=req.params;
+        const{ nombre, objetivo }=req.body;
+
+        //Buscaremos que exista el Proyecto de la ID que nos dieron
+        const proyecto_found = await proyecto.findByPk(proyecto_id)
+        if(!proyecto_found){
+            return res.status(404).json({ message: 'Proyecto no encontrado' })
+        }
+
+        //Actualizamos Datos
+        proyecto_found.nombre = nombre;
+        proyecto_found.objetivo = objetivo;
+
+        //Guardamos
+        await proyecto_found.save();
+        return res.status(200).json({message:'Proyecto Actualizado'});
+
+    }catch(error){
+        console.error('Error actualizando proyecto');
+        res.status(500).json({message: 'Internal Server Error'});
+    }
+}
+
+//DELETE Proyecto
+const deleteProyecto = async (req:Request, res:Response)=>{
+    try{
+
+
+        //Obtenemos ID y parmetros del body
+        const{ proyecto_id }=req.params;
+        
+         //Buscaremos que exista el Proyecto de la ID que nos dieron
+         const proyecto_found = await proyecto.findByPk(proyecto_id)
+
+         //Comprobamos que exista
+         if(!proyecto_found){
+                return res.status(404).json({ message: 'Proyecto no encontrado' })
+         }
+
+         //Eliminamos
+         await proyecto.destroy({
+            where:{
+                id:proyecto_id
+            }
+         });
+
+        return res.status(200).json({message: 'Proyecto Eliminado'});
+    }catch(error){
+        console.error('Error actualizando proyecto');
+        res.status(500).json({message: 'Internal Server Error'});
+    }
+}
+
+const getProyecto = async (req:Request, res:Response)=>{
+    try{
+        const proyectos = await proyecto.findAll();
+
+        if(proyectos.length === 0){
+            return res.status(404).json({message:'Aun no hay Proyectos creados'});
+        }
+        return res.json(proyectos);
+    }catch(error){
+        console.error('Error obteniendo proyectos');
+        return res.status(500).json({message:'Internal Server Error'});
+    }
+}
+
+export{ postProyecto, updateProyecto, deleteProyecto, getProyecto }
