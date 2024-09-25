9d997775d3ae9ea776c0579af382eff11991784c
Luis Mario <luismario.lr46@gmail.com>
2024-09-25 11:35:45 -0700

    score

--- src/controllers/score.ts
+++ src/controllers/score.ts
@@ -7,30 +7,43 @@ const score = async (req: Request, res: Response) => {
     const {proyecto_id} = req.params;
     const proyectoActual = await proyecto.findByPk(proyecto_id);
     const criterios = await criterio.findAll({
-        where: {id_proyecto:proyecto_id}
+        where: {id_proyecto: proyecto_id}
     });
     const matrizActual = await matriz.findAll({
-        where: {id_proyecto:proyecto_id}
-    });
-    //SCORE
-    let scoreFinal: any[] = [];
-    matrizActual.forEach(element => {
-        scoreFinal.push()
-        
+        where: {id_proyecto: proyecto_id}
     });
 
+    if (!proyectoActual) {
+        return res.status(404).json({ message: 'El proyecto no existe' });
+    }
+    if (!criterios || criterios.length === 0) {
+        return res.status(404).json({ message: 'El proyecto no tiene criterios' });
+    }
+    if (!matrizActual || matrizActual.length === 0) {
+        return res.status(404).json({ message: 'El proyecto no tiene matriz' });
+    }
 
+    
+    const scores: any = {};
 
-
-
-
-
-
+    matrizActual.forEach((matrizItem) => {
+        const criterioActual = criterios.find(c => c.id === matrizItem.id_criterio);
+        if (criterioActual) {
+            // multiplicacion
+            const weightedValue = matrizItem.valor * criterioActual.peso;
+            //acumulacion
+            if (!scores[matrizItem.id_alternativa]) {
+                scores[matrizItem.id_alternativa] = 0;
+            }
+            scores[matrizItem.id_alternativa] += weightedValue;
+        }
+    });
 
     return res.status(201).json({
-        matriz:matrizActual,
-        criterioss:criterios});
-}
+        matriz: matrizActual,
+        criterios: criterios,
+        scores
+    });
+};
 
-
-export {score}
+export { score };
