5d23b09647c3004d608cf91bbc389af66eccfe66
gusvaldev <gavq0603@gmail.com>
2024-10-01 20:03:15 -0700

    formulario alternativas terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router'; // Importa RouterModule
 import { ProyectosComponent } from './proyectos/proyectos.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [ProyectosComponent, RouterModule] // Asegúrate de importar RouterModule
+  imports: [ProyectosComponent, RouterModule, HttpClientModule] // Asegúrate de importar RouterModule
 })
 export class AppComponent {
   selectOption: string = "";
